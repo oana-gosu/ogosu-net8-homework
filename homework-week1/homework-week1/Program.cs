4fd806b3fea766110a46ce33d60a382d429b9bda
oana-gosu <oana-gosu@users.noreply.github.com>
2023-06-18 13:18:40 +0300

    w1 tema2

---
+++ homework-week1/homework-week1/Program.cs
@@ -0,0 +1,51 @@
+﻿//exercise1
+using System.Numerics;
+Console.WriteLine("Input x: ");
+string inputedx = Console.ReadLine();
+int x = Convert.ToInt32(inputedx);
+Console.WriteLine("Input y: ");
+string inputedy = Console.ReadLine();
+int y = Convert.ToInt32(inputedy);
+int Addition = x + y;
+Console.WriteLine(Addition);
+int Substraction = x - y;
+Console.WriteLine(Substraction);
+int Division = x / y;
+Console.WriteLine(Division);
+int Multiplication = x * y;
+Console.WriteLine(Multiplication);
+int Modulo = x % y;
+Console.WriteLine(Modulo);
+
+//exercise2
+int a = 7 + 20;
+Console.WriteLine(a);
+int b = 25 - 30;
+Console.WriteLine(b);
+float c = 40 / 3f;
+Console.WriteLine(c);
+int d = 7 % 3;
+Console.WriteLine(d);
+Console.WriteLine(++a);
+Console.WriteLine(--b);
+int e = 21;
+e += 7;
+Console.WriteLine(e);
+e -= 2;
+Console.WriteLine(e);
+
+//exercise 3
+Console.WriteLine(5 == 23);
+Console.WriteLine(254 != 7522);
+Console.WriteLine(22 == 22 && 73!= 84);
+Console.WriteLine(true == true || true != true);
+Console.WriteLine(!(2563 != 49845));
+
+
+
+
+
+
+
+
+
